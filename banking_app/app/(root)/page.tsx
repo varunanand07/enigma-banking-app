09769e66f2066a2cf04adea3f7ef88841d09d4c9
varunanand07 <anandvarun2003@gmail.com>
2024-12-20 22:14:58 +0000

    Refactor Home page to use custom fonts and add HeaderBox component

--- banking_app/app/(root)/page.tsx
+++ banking_app/app/(root)/page.tsx
@@ -1,10 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Varun' };
+    
   return (
-    <div>
-      
+    <section className="home">Home
+    <div className="home-content">
+        <header className="home-header">
+            <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and Manage your account and trascations efficiently"
+            />
+
+            <TotalBalanceBox
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1250.35}
+            />
+        </header>
+
     </div>
+    </section>
   )
 }
 
